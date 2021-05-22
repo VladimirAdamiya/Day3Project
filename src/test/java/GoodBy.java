487834ba6e50e869f4e99ebfe1e97ec4e61fa5fe
VladimirAdamiya <adamiya.v@gmail.com>
2021-05-22 11:43:19 -0500

    committing all together

---
+++ src/test/java/GoodBy.java
@@ -0,0 +1,6 @@
+public class GoodBy {
+    // work 3 is done
+    // work 4 is done
+    // work 5 is done after resetting hard way
+    // committing all together
+}
--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,6 +3,10 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         // enhancing hello
+        // work 1
+        // work 2
+
+
     }
 
 }
